0b42080d57ea2cf9e0043e0fb2008c78a781c0fa
ThearyCoding <chorntheary12@gmail.com>
2025-04-30 22:16:43 +0700

    add get all users features

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
+
+router.get("/", async (req,res) => {
+    try {
+        const users = await User.find();
+
+        res.json(users);
+    } catch (error) {
+        
+    }
+})
 module.exports = router;
